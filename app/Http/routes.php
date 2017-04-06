e78a84e360b0e539a0d603a5cbad8f1ee1db837e
PengYuan Chen <yuanchen@kmu.edu.tw>
2017-04-06 16:51:58 +0800

    學習ROUTE基本用法

---
+++ app/Http/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return "Page: About";
+});
+
+Route::get('/contact', function () {
+    return "Page: Contact";
+});
+
+Route::get('/post/{id}/{name}', function ($id,$name) {
+    return "Page: Contact" . $id .",Name:" . $name;
+});
+
+// Route::get('/demo', function () {
+//     //return view('welcome');
+//     return "Hello World";
+// });
